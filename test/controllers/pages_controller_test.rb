c4df5e495309d9064cd563ba41f90914c67de8e7
Rafael Quinteros <rafael.quinterosvq@gmail.com>
2021-10-21 20:02:06 -0300

    agregue la pagina de test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get pages_index_url
+  test "should get test" do
+    get pages_test_url
     assert_response :success
   end
 
